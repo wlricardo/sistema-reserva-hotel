527f23907066ea38eb6bd554c1b9b8ecab231e4b
wlricardo <wlricardo@gmail.com>
2022-03-02 18:50:10 -0300

    Implementação das Exception classes

--- src/exceptions/ReservaException.java
+++ src/exceptions/ReservaException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
-public class ReservaException {
+public class ReservaException extends RuntimeException {
+    
+	private static final long serialVersionUID = 1L;
 
+	public ReservaException(String msg) {
+        super(msg);
+    }    
 }
--- src/exceptions/ValorException.java
+++ src/exceptions/ValorException.java
@@ -1,5 +1,10 @@
 package exceptions;
 
-public class ValorException {
+public class ValorException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
+	public ValorException(String msg) {
+		super(msg);
+	}
 }
