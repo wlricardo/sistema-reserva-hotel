ce1c443aa4808da210f72e05249d223f292b666d
wlricardo <wlricardo@gmail.com>
2022-03-02 19:35:01 -0300

    Implementação da classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+import actions.Input;
+import actions.Menu;
+import exceptions.ValorException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		do {
+			do {
+				try {
+					Menu.principal();
+					int opcao = Input.escolherOpcaoMenu();
+					break;
+				} catch (InputMismatchException e) {
+					System.out.println("\n ** Erro! Valor inválido. Tente novamente **\n");
+				} catch (ValorException e) {
+					System.out.println(e.getMessage());
+				}
+
+			} while (true);
+		} while (Input.continuar().toUpperCase().equals("S"));
+
+		sc.close();
 	}
-
 }
