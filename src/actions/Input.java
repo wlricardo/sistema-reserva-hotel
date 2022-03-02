b8238d7c7219a105c8ea8d5491b3ca44abb6aa3c
wlricardo <wlricardo@gmail.com>
2022-03-02 19:02:09 -0300

    Implementação das classes Menu e Input

--- src/actions/Input.java
+++ src/actions/Input.java
@@ -1,5 +1,23 @@
 package actions;
 
+import java.util.Scanner;
+
+import exceptions.ValorException;
+
 public class Input {
 
+	@SuppressWarnings("resource")
+	public static int escolherOpcaoMenu() {
+		int op = new Scanner(System.in).nextInt();
+		if (op < 0 || op > 3) {
+			throw new ValorException("\n ** Erro! Forneça valores entre [0-3] **\n");
+		}
+		return op;
+	}
+
+	@SuppressWarnings("resource")
+	public static String continuar() {
+		System.out.print("Deseja continuar [S/N] ? : ");
+		return new Scanner(System.in).nextLine();
+	}
 }
--- src/actions/Menu.java
+++ src/actions/Menu.java
@@ -1,5 +1,17 @@
 package actions;
 
-public class Menu {
+public abstract class Menu {
 
+	public static void principal() {
+		System.out.println("\n--------------------------------------");
+		System.out.println("           HOTEL 123 LÉGUAS			  ");
+		System.out.println(" Sistema de Gerenciamento de Reservas ");
+		System.out.println("--------------------------------------\n");
+		System.out.println("Escolha uma opção:");
+		System.out.println("[1] Cadastrar hóspedes");
+		System.out.println("[2] Remover hóspedes");
+		System.out.println("[3] Relatório");
+		System.out.println("[0] Sair");
+		System.out.print("> ");
+	}
 }
